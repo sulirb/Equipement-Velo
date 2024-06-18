d7f9d5f2cde9088675129b73f8a7b4090062c6b2
sulirb <121161440+sulirb@users.noreply.github.com>
2024-06-18 14:54:45 +0200

    writer page update

--- app/admin/wr/page.jsx
+++ app/admin/wr/page.jsx
@@ -2,21 +2,26 @@
 import { useCookies } from "react-cookie";
 import MyEditor from "../../../components/editor";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function Article() {
   const [cookies] = useCookies(["token"]);
   const token = cookies.token;
   const router = useRouter();
 
+  useEffect(() => {
+    if (!token) {
+      router.push("/error");
+    }
+  }, [token, router]);
+
+  if (!token) {
+    return null;
+  }
+
   return (
     <div>
-      {token ? (
-        <MyEditor />
-      ) : (
-        setTimeout(() => {
-          router.push("/error");
-        })
-      )}
+      <MyEditor />
     </div>
   );
 }
