aef3ebbf92ecb6642301d8e7539747a709680c1d
sulirb <121161440+sulirb@users.noreply.github.com>
2024-07-03 13:44:35 +0200

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import { Montserrat } from "next/font/google";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "600", "700"],
@@ -17,6 +19,8 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
       <GoogleAnalytics gaId="G-JNFF13MLTT" />
     </html>
