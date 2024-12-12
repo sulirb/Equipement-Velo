6b88b87a44ea649a5dbd6a33cc75391beafd4df3
sulirb <121161440+sulirb@users.noreply.github.com>
2024-12-12 22:05:40 +0100

    add maintenance mode

---
+++ app/maintenance/page.jsx
@@ -0,0 +1,17 @@
+import "./maintenance.scss";
+
+const MaintenancePage = () => {
+  return (
+    <div className="maintenance-page">
+      <div className="maintenance-content">
+        <h1>Site en Maintenance</h1>
+        <p>
+          Nous effectuons actuellement une maintenance. Merci de revenir plus
+          tard.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MaintenancePage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,10 +14,25 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  async redirects() {
+    // Vérifiez si le mode maintenance est activé
+    if (process.env.MAINTENANCE_MODE === true) {
+      return [
+        {
+          source: "/((?!maintenance).*)",
+          destination: "/maintenance",
+          permanent: false,
+        },
+      ];
+    }
+
+    // Retournez un tableau vide si le mode maintenance n'est pas activé
+    return [];
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+  setupDevPlatform();
 }
 
 export default nextConfig;
